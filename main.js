d6c90b019cae1635e9ae3623d6315bd7b4fe4406
Adwaita Raj Modak <adwaitaraj@gmail.com>
2021-04-05 09:58:54 +0530

    worked

---
+++ main.js
@@ -0,0 +1,6 @@
+const selectBox = document.querySelector(".select-box");,
+SelectXBtn = selectBox.querySelector(".player1");,
+SelectOBtn = selectBox.querySelector(".player2");
+
+
+window.onload=()
